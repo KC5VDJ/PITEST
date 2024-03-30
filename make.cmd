48a42f029bdb6b265f786e0cb1cf1098ca347640
KC5VDJ <136194499+KC5VDJ@users.noreply.github.com>
2024-03-30 08:56:16 -0600

    Create make.cmd

---
+++ make.cmd
@@ -0,0 +1,2 @@
+ifort /fast /QxCORE-AVX2 /arch:CORE-AVX2 /Qip /Qunroll /O3 /Qparallel /threads /Qvec /Qsimd /Qsimd-serialize-fp-reduction /Qopt-report3 /Qopt-report-file:pitest.opt.txt -o pitest.exe pitest.f 
+  
